6189bf163989031cd29cc44029973ed00bfc414b
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-06 16:36:10 +0100

    inserito scritta in pagina con immagine

---
+++ vue.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+el: '#root',
+  data: {
+    message: 'Hello world',
+    mioValore: 'ciao',
+    myClass: 'title',
+    mySource: 'https://i.picsum.photos/id/913/200/200.jpg?hmac=MQWqYyJuxoagkUNdhY5lwuKw7QwcqzMEm4otshKpUWQ',
+  },
+
+  methods: {
+    imgChange: function () {
+      
+    }
+  }
+});
